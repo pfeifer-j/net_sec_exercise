771effe9732a464fb6c20b2f48da5c916f3b013e
Julian Weske <julian.weske_extern1@ppi.de>
2024-06-05 19:16:56 +0200

    added threading 2

--- ex4/Https_proxy.py
+++ ex4/Https_proxy.py
@@ -37,10 +37,10 @@ def main():
 
     while True:
         sock_for_browser, fromaddr = client_sock.accept()
-        sock_for_browser = context_server.wrap_socket(sock_for_browser,
+        ssock_for_browser = context_server.wrap_socket(sock_for_browser,
                                                server_side=True)
         x = threading.Thread(target=process_request,
-                             args=(sock_for_browser,))
+                             args=(ssock_for_browser,))
         x.start()
 
         # Accept incoming connections from the client
@@ -52,54 +52,29 @@ def main():
         #with context_server.wrap_socket(client_conn, server_side=True) as ssock_client:
         #    client_data = ssock_client.recv(4096)
 
-        # Create a socket for the server
-        # server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # server_sock.connect((SERVER_HOST, SERVER_PORT))
-
-        # Wrap the server socket in TLS
-        # server_conn = context.wrap_socket(server_sock, server_side=False)
-
-        # Forward client data to the server
-        # server_conn.sendall(client_data)
-            # Create a socket for the server
-         #   with socket.create_connection((SERVER_HOST, SERVER_PORT)) as server_sock:
-          #      with context_client.wrap_socket(
-           #         server_sock, server_hostname=SERVER_HOST
-            #    ) as ssock_server:
-                    # Forward client data to the server
-             #       ssock_server.sendall(client_data)
-
-        # Receive response from the server
-        # server_resp = server_conn.recv(4096)
-                    # Receive response from the server
-        #            server_resp = ssock_server.recv(4096)
-
-        # Forward server response to the client
-        # client_conn.sendall(server_resp)
-
-        # Close connections
-        # server_conn.close()
-        # client_conn.close()
-        # Forward server response to the client
-        #ssock_client.sendall(server_resp)
 
 
-def process_request(ssock_for_browser):
-    hostname = 'www.example.com'
+
+def process_request(ssock_for_browser, context_client):
     # Make a connection to the real server
-    sock_for_server = socket.create_connection((hostname, 443))
-    # ssock_for_server = ...  # [Code omitted]: Wrap the socket using TLS
+    server_sock = socket.create_connection((SERVER_HOST, SERVER_PORT))
+    sock_for_server = context_client.wrap_socket(
+        server_sock, server_hostname=SERVER_HOST
+    )
     request = ssock_for_browser.recv(2048)
+
     if request:
         # Forward request to server
         sock_for_server.sendall(request)
-    # Get response from server, and forward it to browser
-    response = sock_for_server.recv(2048)
-    while response:
-        ssock_for_browser.sendall(response)  # Forward to browser
         response = sock_for_server.recv(2048)
+
+        # Get response from server, and forward it to browser
+        while response:
+            ssock_for_browser.sendall(response)  # Forward to browser
+            response = sock_for_server.recv(2048)
     ssock_for_browser.shutdown(socket.SHUT_RDWR)
     ssock_for_browser.close()
+
 
 
 
